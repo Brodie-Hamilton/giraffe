e04b9b52c35dcdaf8372af857c26394b6cdc2b16
Brodie Hamilton <35979029+Brodie-Hamilton@users.noreply.github.com>
2023-07-03 03:44:28 +0000

    Update 5 files

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,17 @@ import Header from "./Header";
  */
 const dataSources: Readonly<DataSource[]> = Object.freeze<DataSource[]>([
   // A mini version for testing and small rendering verification.
-  { file: "athletes20.csv", name: "athletes20.csv", title: "Athletes (20)" },
+  {
+    file: "athletes-20.csv",
+    name: "athletes20.csv",
+    title: "Athletes (20)",
+  },
   // The default data, as per the task AC.
-  { file: "athletes10k.csv", name: "athletes10k.csv", title: "Athletes (10k)" },
+  {
+    file: "athletes-10k.csv",
+    name: "athletes10k.csv",
+    title: "Athletes (10k)",
+  },
   // An amped-up input data source to test the performance of the
   // implementation when dealing with a data set of x10.
   // { file: athletes250k, name: "athletes250k.csv", title: "Athletes (250k)" }
@@ -29,7 +37,7 @@ const dataSources: Readonly<DataSource[]> = Object.freeze<DataSource[]>([
  * App entry point.
  */
 export default function App() {
-  const [dataSource, setDataSource] = useState<DataSource>(dataSources[1]);
+  const [dataSource, setDataSource] = useState<DataSource>(dataSources[0]);
   const selectDataSource = (name: string) => {
     setDataSource(dataSources.find((ds) => ds.name === name) as DataSource);
   };
